8d76ac6db3ca5a2e4e7cdd37e329e33ba96ec6b8
rajavi-mishra <59727634+rajavi-mishra@users.noreply.github.com>
2021-04-16 09:41:30 -0700

    Update umba_2.sql

--- umba_2.sql
+++ umba_2.sql
@@ -4,6 +4,15 @@ loans: id, created, loan_status, user_id, amount
 transactions: transaction.id, created, load_id, user_id, transaction_fee, t_type, transaction.amount 
 */
 
+/*Process:
+    1) Select columns 
+    2) Aggregate them appropriately 
+    2) Rename them appropriately
+    3) These columns should be selected them from the appropriate table 
+    4) Group them keeping the aggregation in mind
+    5) Order the table appropriately    
+    */
+    
 SELECT cast((date_part('month', created)) AS INTEGER) AS month, 
     COUNT(*), SUM(amount) AS total_loan_amount, 
     ROUND(AVG(amount), 2) AS average_loan_size, 
@@ -15,6 +24,6 @@ ORDER BY
     case when date_part('month', created) > date_part('month', CURRENT_TIMESTAMP) then date_part('month', created) end DESC,
     case when date_part('month', created) = date_part('month', CURRENT_TIMESTAMP) then date_part('month', created) end ASC; 
 
-/*using case here to sort by the most recent mony, used current timestamp to get the current month instead of hardcoding the month to 4*/
+/*using case here to sort by the most recent month, used current timestamp to get the current month instead of hardcoding the month to 4 i.e. April*/
 
     
